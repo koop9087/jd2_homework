0ba165895af427aedc98071230561859f1acc42d
koop9087 <yarik4mo>
2021-10-24 00:08:10 +0300

    hw2

--- task2_4/src/main/java/Main.java
+++ task2_4/src/main/java/Main.java
@@ -1,35 +1,41 @@
+import data.DataSource;
+
 import java.sql.*;
 
 public class Main {
-    private static final String URL = "jdbc:mysql://localhost:3306/task2_4?serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Root";
-
+    protected static DataSource dataSource;
+    //Запуск программы с помощью java -classpath d:\work\jd2_homework\task2_4\target\classes;c:\Users\home\.m2\repository\mysql\mysql-connector-java\8.0.26\mysql-connector-java-8.0.26.jar Main 2020-10-12 4 3 8
     public static void main(String... args) {
         String argDate = args[0];
         String argReceiver = args[1];
         int newReceiver = Integer.parseInt(argReceiver);
         String values = args[2];
         double valueArg = Double.parseDouble(values);
+        String argNum = args[3];
+        int numReceiver = Integer.parseInt(argNum);
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            Statement statement = connection.createStatement();
-            String dateQuery = "('" + argDate + "'" + "," + newReceiver + "," + valueArg + ")";
-            String query = "INSERT INTO task2_4.expenses (paydate, receiver, value) \n" +
+            dataSource = new DataSource();
+            Statement statement = dataSource.getConnection().createStatement();
+            String dateQuery = "('" + argDate + "'" + "," + newReceiver + "," + valueArg + "," + numReceiver + ")";
+            String query = "INSERT INTO task2_4.expenses (paydate, receiver, value, num) \n" +
                     "values" + dateQuery + ";";
-            int res = statement.executeUpdate(query);
+            statement.executeUpdate(query);
+            String template = "SELECT * FROM expenses WHERE value < ?";
+            PreparedStatement ps = dataSource.getConnection().prepareStatement(template);
+            ps.setFloat(1, 200F);
+            ps.executeQuery();
             ResultSet resultPrint = statement.executeQuery("SELECT * FROM expenses;");
             while (resultPrint.next()) {
                 String paydate = resultPrint.getString(2);
                 double value = resultPrint.getDouble(3);
                 String name = resultPrint.getString(4);
-                System.out.println(paydate + " " + value + " " + name);
+                int num = resultPrint.getInt(1);
+                System.out.println(num + " " + paydate + " " + value + " " + name);
             }
             resultPrint.close();
             statement.close();
-            connection.close();
-        } catch (ClassNotFoundException | SQLException e) {
+            dataSource.getConnection().close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
