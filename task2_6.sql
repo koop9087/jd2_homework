503104121e80ea0b599431685bbc1f22ad442a4d
koop9087 <yarik4mo>
2021-11-22 11:14:14 +0300

    homework 2 reworked

--- task2_6.sql
+++ task2_6.sql
@@ -1,26 +1,26 @@
-create database if not exists task9;
-create table if not exists task9.receivers(
+create database if not exists task6;
+create table if not exists task6.receivers(
 id int auto_increment primary key,
 name varchar(255) not null
 );
-create table if not exists task9.expenses(
+create table if not exists task6.expenses(
 id int (10) auto_increment primary key,
 paydate date not null,
 receiver int (12) not null,
 value dec (8,3) not null,
 constraint receiver foreign key (receiver) references receivers(id)
 );
-insert into task9.receivers (name) values ("prostor");
-insert into task9.receivers (name) values ("walmart");
-insert into task9.receivers (name) values ("grin");
-insert into task9.expenses (id,paydate,receiver,value)
-values (1, '10.11.2012',1,78.40);
-insert into task9.expenses (id,paydate,receiver,value)
+insert into task6.receivers (name) values ("prostor");
+insert into task6.receivers (name) values ("walmart");
+insert into task6.receivers (name) values ("grin");
+insert into task6.expenses (id,paydate,receiver,value)
+values (1, '2012.10.11',1,78.40);
+insert into task6.expenses (id,paydate,receiver,value)
 values (2, '2013.12.08',2,99.82);
-insert into task9.expenses (paydate,receiver,value)
+insert into task6.expenses (paydate,receiver,value)
 values ('2019.05.01',3,81.21);
-insert into task9.expenses (paydate,receiver,value)
+insert into task6.expenses (paydate,receiver,value)
 values ('2019.05.01',3,8612.91);
-select sum(value) as Потрачено, name from task9.expenses, task9.receivers rs where receiver=rs.id group by receiver;
-select paydate, sum(value) from task9.expenses where paydate=(select paydate from task9.expenses where value=(select max(value) from task9.expenses));
-select paydate, max(value) from task9.expenses where paydate=(select paydate from task9.expenses where value=(select max(value) from task9.expenses));
+select sum(value) as Потрачено, name from task6.expenses, task6.receivers rs where receiver=rs.id group by receiver;
+select paydate, sum(value) from task6.expenses where paydate=(select paydate from task6.expenses where value=(select max(value) from task6.expenses));
+select paydate, max(value) from task6.expenses where paydate=(select paydate from task6.expenses where value=(select max(value) from task6.expenses));
